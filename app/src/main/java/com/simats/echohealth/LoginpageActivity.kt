6c871e2d5cbb10b51407a280e32b0c4e5febcb69
Bandipottigarinaveen <bandipottigarinaveen@gmail.com>
2025-07-29 19:32:34 +0530

    Push all projrct files

--- app/src/main/java/com/simats/echohealth/LoginpageActivity.kt
+++ app/src/main/java/com/simats/echohealth/LoginpageActivity.kt
@@ -6,9 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.content.Intent
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class LoginpageActivity : AppCompatActivity() {
+    lateinit var loginBtn : Button
+    lateinit var forgotpassBtn : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,6 +26,14 @@ class LoginpageActivity : AppCompatActivity() {
         val signUpText = findViewById<TextView>(R.id.signIn)
         signUpText.setOnClickListener {
             val intent = Intent(this, createaccount::class.java)
+            startActivity(intent)
+        }
+        loginBtn.setOnClickListener {
+            val intent = Intent(this, dashboard::class.java)
+            startActivity(intent)
+        }
+        forgotpassBtn.setOnClickListener {
+            val intent = Intent(this, forgotpassword_page::class.java)
             startActivity(intent)
         }
     }
